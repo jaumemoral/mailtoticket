682817e38fdada3eda71af1d1c83ea6f1d535548
Jaume Moral <jaume.moral@upc.edu>
2023-01-18 13:36:23 +0100

    Syntax error

--- fetchgmail/fetchgmail.sh
+++ fetchgmail/fetchgmail.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true; do 
     python /mailtoticket/fetchgmail/fetchgmail.py
-    if [ "x$HEARTBEAT_URL" !="x" ] then
+    if [ "x$HEARTBEAT_URL" != "x" ]; then
         python /mailtoticket/fetchgmail/heartbeat.py $HEARTBEAT_URL
     fi
     sleep 60

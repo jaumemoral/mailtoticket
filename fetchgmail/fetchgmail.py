0db067b38e623ada5444673585c575edbdbdb5d2
Jaume Moral <jaume.moral@upc.edu>
2026-04-08 13:42:19 +0200

    Canvis per deixar logs quan peta fetchgmail

--- fetchgmail/fetchgmail.py
+++ fetchgmail/fetchgmail.py
@@ -44,22 +44,26 @@ def generate_oauth2_string(username, access_token, base64_encode=True):
 
 if not os.path.exists(settings.token_file):
     print("No tinc el token.pickle creat. Executa el oauth2.sh")
-    exit(0)
+    exit(1)
 
-creds = llegir_token()
-access_token=creds.token
-auth_string=generate_oauth2_string(settings.user, access_token,base64_encode=False)
-imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
-imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
-imap_conn.select('INBOX')
+try:
+    creds = llegir_token()
+    access_token=creds.token
+    auth_string=generate_oauth2_string(settings.user, access_token,base64_encode=False)
+    imap_conn = imaplib.IMAP4_SSL('imap.gmail.com')
+    imap_conn.authenticate('XOAUTH2', lambda x: auth_string)
+    imap_conn.select('INBOX')
 
-resp, items = imap_conn.uid('SEARCH',None, '(UNSEEN)')
-items = items[0].split() # getting the mails id
+    resp, items = imap_conn.uid('SEARCH',None, '(UNSEEN)')
+    items = items[0].split() # getting the mails id
 
-for emailid in items:
-    resp, data = imap_conn.uid('FETCH',emailid, "(RFC822)")    
-    escriure_mail(data[0][1])
-    with open(MAIL_TMP, "rb") as f:
-        r=subprocess.run(settings.mailtoticket,stdin=f) 
-        if r.returncode>0:
-            imap_conn.uid('STORE', emailid, '-FLAGS', '(\Seen)')
+    for emailid in items:
+        resp, data = imap_conn.uid('FETCH',emailid, "(RFC822)")    
+        escriure_mail(data[0][1])
+        with open(MAIL_TMP, "rb") as f:
+            r=subprocess.run(settings.mailtoticket,stdin=f) 
+            if r.returncode>0:
+                imap_conn.uid('STORE', emailid, '-FLAGS', '(\Seen)')
+except Exception as e:
+    print("No puc connectar: ", e)
+    exit(1)
--- fetchgmail/fetchgmail.sh
+++ fetchgmail/fetchgmail.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 while true; do 
-    python /mailtoticket/fetchgmail/fetchgmail.py
+    python /mailtoticket/fetchgmail/fetchgmail.py 2>&1 > /tmp/mailtoticket.log
+    STATUS=$?
     if [ "x$HEARTBEAT_URL" != "x" ]; then
-        python /mailtoticket/fetchgmail/heartbeat.py $HEARTBEAT_URL
+        if [ "x$STATUS" == "x0" ]; then
+            python /mailtoticket/fetchgmail/heartbeat.py $HEARTBEAT_URL
+        fi
+    fi
+    if [ "x$HEARTBEAT_URL_ERROR" != "x" ]; then
+        if [ "x$STATUS" != "x0" ]; then
+            python /mailtoticket/fetchgmail/heartbeat.py $HEARTBEAT_URL_ERROR
+        fi
     fi
     sleep 60
 done
